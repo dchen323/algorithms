8b7c43a211863e40de12e265b9b411a75869180f
Daniel <d.chen323@gmail.com>
2018-07-03 16:49:35 -0700

    Create hashing for string array and hashes

--- Hash Map:LRC Cache/lib/p02_hashing.rb
+++ Hash Map:LRC Cache/lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    map.with_index {|el,idx| el.to_s.ord**idx}.inject(:+).hash
   end
 end
 
 class String
   def hash
+    chars.map.with_index {|el,idx| el.ord*idx}.inject(:+).hash
   end
 end
 
@@ -16,6 +18,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    each {|k,v| sum+= k.to_s.ord * v.to_s.ord}
+    sum.hash
   end
 end
