14ebd3a0f7318765e8be53f73af9b840c15e35e0
Daniel <d.chen323@gmail.com>
2018-07-04 17:47:04 -0700

    Finish heap set

--- Heaps/lib/heap.rb
+++ Heaps/lib/heap.rb
@@ -4,18 +4,25 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
-  end
-
-  def count
+    @store = []
+    @count = 0
+    @prc = prc
   end
 
   def extract
+    @store[0], @store[-1] = @store[-1], @store[0]
+    value = @store.pop
+    self.class.heapify_down(@store,0)
+    value
   end
 
   def peek
   end
 
   def push(val)
+    @store.push(val)
+    self.class.heapify_up(store,@count)
+    @count += 1
   end
 
   public
@@ -33,16 +40,20 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_index, len = array.length, &prc)
+    prc ||= Proc.new {|el1,el2| el1 <=> el2}
     index = parent_index * 2
-    original_root = array[0]
     child_one = array[index + 1]
     child_two = array[index + 2]
-    until child_one == nil && child_two == nil ||
-          original_root <= child_one && original_root <= child_two
-            swap_index = child_one < child_two ? index + 1 : index + 2
-            original_root, array[swap_index] = array[swap_index], original_root
-            debugger
-            parent_idx = swap_index
+    until (child_one == nil && child_two == nil) ||
+          (child_one && !child_two && prc.call(array[parent_index],child_one) == -1) ||
+          (prc.call(array[parent_index],child_one) == -1 && prc.call(array[parent_index],child_two) == -1)
+            if child_two
+              swap_index = prc.call(child_one,child_two) == -1 ? index + 1 : index + 2
+            else
+              swap_index = index + 1
+            end
+            array[parent_index], array[swap_index] = array[swap_index], array[parent_index]
+            parent_index = swap_index
             index = parent_index * 2
             child_one = array[index + 1]
             child_two = array[index + 2]
@@ -50,6 +61,14 @@ class BinaryMinHeap
    array
   end
 
-  def self.heapify_up(array, child_idx, len = array.length, &prc)
+  def self.heapify_up(array, child_index, len = array.length, &prc)
+    prc ||= Proc.new {|el1,el2| el1 <=> el2}
+    parent_index = self.parent_index(child_index) unless child_index == 0
+    until child_index == 0 || prc.call(array[child_index],array[parent_index]) == 1
+      array[child_index], array[parent_index] = array[parent_index], array[child_index]
+      child_index = parent_index
+      parent_index = self.parent_index(child_index) unless child_index == 0
+    end
+    array
   end
 end
