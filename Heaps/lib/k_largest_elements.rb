59c578ca476b0a4ff952673dfa3af81af0d72f30
Daniel <d.chen323@gmail.com>
2018-07-04 19:02:51 -0700

    Find the largets k element

--- Heaps/lib/k_largest_elements.rb
+++ Heaps/lib/k_largest_elements.rb
@@ -1,5 +1,16 @@
 require_relative 'heap'
+require 'byebug'
 
 def k_largest_elements(array, k)
-
+  prc ||= Proc.new {|el,el2| -1 * (el <=> el2)}
+  (0...array.length).each do |idx|
+    BinaryMinHeap.heapify_up(array,idx,&prc)
+  end
+  idx = array.length - 1
+  while idx > 0
+    array[0], array[idx] = array[idx], array[0]
+    idx -= 1
+    BinaryMinHeap.heapify_down(array,0,idx, &prc)
+  end
+  array.drop(array.length - k)
 end
