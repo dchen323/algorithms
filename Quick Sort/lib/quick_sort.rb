eb5785502997db85954ecb703f7ec0a47823aea9
Daniel <d.chen323@gmail.com>
2018-07-05 18:59:51 -0700

    Complete quick sort not in place

--- Quick Sort/lib/quick_sort.rb
+++ Quick Sort/lib/quick_sort.rb
@@ -4,6 +4,19 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length < 2
+    pivot = rand(array.length - 1)
+    left = []
+    right = []
+    array.each_index do |idx|
+      next if idx == pivot
+      if array[idx] < array[pivot]
+        left.push(array[idx])
+      elsif array[idx] > array[pivot]
+        right.push(array[idx])
+      end
+    end
+    self.class.sort1(left).concat([array[pivot]]).concat(self.class.sort1(right))
   end
 
   # In-place.
