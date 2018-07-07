6db055fd2554167b437e17f91e5589a31eec7498
Daniel <d.chen323@gmail.com>
2018-07-07 13:29:23 -0700

    Finish insertion of nodes in binary search tree

--- Binary Search Tree/lib/binary_search_tree.rb
+++ Binary Search Tree/lib/binary_search_tree.rb
@@ -1,11 +1,24 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
+require 'byebug'
 
 class BinarySearchTree
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if !@root
+      set_root(value)
+    else
+      node = traverse_tree(value)
+      if value > node.value
+        node.right = BSTNode.new(value)
+      elsif value < node.value
+        node.left = BSTNode.new(value)
+      end
+    end
   end
 
   def find(value, tree_node = @root)
@@ -19,7 +32,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -27,8 +40,24 @@ class BinarySearchTree
   def in_order_traversal(tree_node = @root, arr = [])
   end
 
+  attr_reader :root
 
   private
   # optional helper methods go here:
+  def set_root(value)
+    @root = BSTNode.new(value)
+  end
 
+  def traverse_tree(value)
+    start_node = @root
+    until start_node.left == nil && start_node.value > value ||
+          start_node.right == nil && start_node.value < value
+            if start_node.value > value
+              start_node = start_node.left
+            elsif start_node.value < value
+              start_node = start_node.right
+            end
+    end
+    start_node
+  end
 end
--- Binary Search Tree/lib/bst_node.rb
+++ Binary Search Tree/lib/bst_node.rb
@@ -1,8 +1,11 @@
 class BSTNode
   def initialize(value)
     @value = value
+    @left = nil
+    @right = nil
+    @parent = nil
   end
 
   attr_reader :value
-  attr_accessor :left, :right
+  attr_accessor :left, :right, :parent
 end
